1baca06118592eb9f4373a5055b49311c11c332e
ZakuroSoda <reyesleeyh@gmail.com>
2023-12-28 10:36:56 +0800

    cool

--- src/Account/Login/Login.js
+++ src/Account/Login/Login.js
@@ -1,9 +1,30 @@
 import React from 'react';
+import './Login.css';
+import arrow from './arrow.svg';
+
 
 function Login() {
   return (
     <div className="Login">
-      <h1>Login</h1>
+      <div className="login-header">Login</div>
+      <div className="login-desc">Please enter your e-mail and password to continue:</div>
+      <form className="login-form">
+        <div className="login-form-row">
+          <input className="login-form-input" type="email" id="email" name="email" placeholder=" " autoComplete="off" required/>
+          <label className="login-form-label" htmlFor="email">
+            Email
+          </label>
+        </div>
+        <div className="login-form-row">
+          <input className="login-form-input" type="password" id="password" name="password" placeholder=" " autoComplete="off" required/>
+          <label className="login-form-label" htmlFor="password">
+            Password
+          </label>
+        </div>
+        <div className="login-form-row">
+          <a className="login-form-submit"><img src={arrow} className="login-form-submit-icon" /></a>
+        </div>
+      </form>
     </div>
   );
 }
